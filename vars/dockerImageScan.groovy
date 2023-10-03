6c986686efa401c59da887bdd9ce2635cac08442
Swastik <87716096+swasdas@users.noreply.github.com>
2023-10-03 11:13:46 +0900

    Update dockerImageScan.groovy

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,6 +1,7 @@
-def call(String ImgName, String DockerHubUser){
+def call(String ImgName, String ImgTag, String DockerHubUser){
   sh """
     trivy image ${DockerHubUser}/${ImgName}:latest > scan.txt 
+    //if you want image tag version:-  ${DockerHubUser}/${ImgName}:${ImgTag}
     cat scan.txt 
   """
 }
