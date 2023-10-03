a2278eed6e8d539fbc3ca19e8545faeb290f9384
Swastik <87716096+swasdas@users.noreply.github.com>
2023-10-03 11:40:45 +0900

    Update mvnIntegrationTest.groovy
    comment in meaning

--- vars/mvnIntegrationTest.groovy
+++ vars/mvnIntegrationTest.groovy
@@ -1,3 +1,6 @@
 def call(){
   sh 'mvn verify -DskipUnitTests'
 }
+
+// execute integration tests (in src/it directory) means testing interactions between different components or systems
+// and skip running unit tests (in the src/test directory) during the build process 
