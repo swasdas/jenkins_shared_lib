59ee0da037d8158bb2d5780f3091cdf66d236d9c
Swastik <87716096+swasdas@users.noreply.github.com>
2023-10-03 11:37:02 +0900

    Update mvnBuild.groovy
    comment meaning

--- vars/mvnBuild.groovy
+++ vars/mvnBuild.groovy
@@ -1,4 +1,7 @@
 def call(){
   sh 'mvn clean install'
-  // this command will build and install a project
 }
+
+// Maven will clean the previous build's output by removing the target directory
+// and then build & install project (compile project, run tests, package application into JAR or WAR file, 
+// and then copy the packaged artifact into the local repository of your Maven setup)
