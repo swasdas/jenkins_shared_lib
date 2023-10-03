0e5d9875d0078b731ee34f741a20dee558e2c82c
Swastik <87716096+swasdas@users.noreply.github.com>
2023-10-03 11:45:58 +0900

    Update mvnBuild.groovy

--- vars/mvnBuild.groovy
+++ vars/mvnBuild.groovy
@@ -4,4 +4,4 @@ def call(){
 
 // Maven will clean the previous build's output by removing the target directory
 // and then build & install project (compile project, run tests, package application into JAR or WAR file, 
-// and then copy the packaged artifact into the local repository of your Maven setup)
+// and then copy the packaged artifact into the local repo of your Maven setup i.e. in target folder)
