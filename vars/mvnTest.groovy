39d69a550bbe7f27909bf45666eaf4837de64f04
Swastik <87716096+swasdas@users.noreply.github.com>
2023-10-03 11:32:52 +0900

    Update mvnTest.groovy
    meaning in comment

--- vars/mvnTest.groovy
+++ vars/mvnTest.groovy
@@ -1,3 +1,7 @@
 def call(){
   sh 'mvn test'
-}
+}  
+  // this command will run the tests for a Java project
+  // Maven will compile your project in source code (src/main) or test code (src/test) directory
+  // and then run the tests found in the project's src/test directory
+  // After running the tests, it generate reports and output the results to the console
