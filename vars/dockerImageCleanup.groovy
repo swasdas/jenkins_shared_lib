b36395d580eae3b7c06dd06d53b9705f3f270be0
Swastik <87716096+swasdas@users.noreply.github.com>
2023-10-04 11:37:46 +0900

    Create dockerImageCleanup.groovy

---
+++ vars/dockerImageCleanup.groovy
@@ -0,0 +1,6 @@
+def call(String ImgName, String ImgTag, String DockerHubUser){ 
+    sh """
+     docker rmi ${DockerHubUser}/${ImgName}:${ImgTag}
+     docker rmi ${DockerHubUser}/${ImgName} ${DockerHubUser}/${ImgName}:latest
+    """
+}
