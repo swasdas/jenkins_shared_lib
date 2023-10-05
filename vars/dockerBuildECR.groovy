ecd38256b4f3158ca2f303b2e917f94490c1f5d4
Swastik <87716096+swasdas@users.noreply.github.com>
2023-10-05 14:31:06 +0900

    Create dockerBuildECR.groovy

---
+++ vars/dockerBuildECR.groovy
@@ -0,0 +1,7 @@
+def call(String aws_id, String region, String ecr_repo){
+  sh """
+    docker build -t ${ecr_repo} .
+    docker tag ${ecr_repo}:latest ${aws_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repo}:latest 
+  """
+} 
+// 96___34.dkr.ecr.ap-northeast-1.amazonaws.com
