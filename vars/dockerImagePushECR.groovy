fe6173c8b4087cd5d5b16ecd36eb45d02b98e6ce
Swastik <87716096+swasdas@users.noreply.github.com>
2023-10-07 18:19:43 +0900

    Update dockerImagePushECR.groovy

--- vars/dockerImagePushECR.groovy
+++ vars/dockerImagePushECR.groovy
@@ -1,8 +1,8 @@
 def call(String aws_id, String region, String ecr_repo){
   sh """
-    aws configure
-    aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_id}.dkr.ecr.${region}.amazonaws.com
+    docker login -u AWS -p $(aws ecr get-login-password --region ${region}) ${aws_id}.dkr.ecr.${region}.amazonaws.com
     docker push ${aws_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repo}:latest 
   """
 } 
 // 89___56.dkr.ecr.ap-northeast-1.amazonaws.com
+// aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_id}.dkr.ecr.${region}.amazonaws.com
